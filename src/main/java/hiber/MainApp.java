83b87deaa15d5babafe43f83cd1e2d78ff71df0e
MaximBachok <staysoulbyme@gmail.com>
2022-01-15 12:54:24 +0400

    не создавать параметры для запросов, писать запросы сразу в методы у метода createQuery есть перегрузка с двумя параметрами для типизированных запрос сделать без каскадов

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -21,9 +21,9 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("mazda",6)));
-      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("bmv", 5)));
-      userService.add(new User("User7", "Lastname7", "user6@mail.ru", new Car("bmv", 5)));
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("car1",1)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("car2", 2)));
+      userService.add(new User("User7", "Lastname7", "user6@mail.ru", new Car("car3", 3)));
 
 
       List<User> users = userService.listUsers();
@@ -32,10 +32,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println("Car = "+user.getCar());
+         if(user.getCar()!=null){
+            System.out.println("Car = "+user.getCar());
+         }
          System.out.println();
       }
-      List<User> userByCar = userService.getUserByCar("BMV",5);
+      List<User> userByCar = userService.getUserByCar("car1",1);
       System.out.println(userByCar);
 
       context.close();
